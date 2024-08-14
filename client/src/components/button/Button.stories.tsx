2cbf0b1d04d9f4855290b4f74393ab6744c901f0
Pawel1894 <pawepohl@gmail.com>
2024-08-14 17:16:23 +0200

    client: storybook setup, creating first story for button component

---
+++ client/src/components/button/Button.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button as ButtonComponent, ButtonProps } from './Button';
+
+const variantOptions: ButtonProps['variant'][] = ['text', 'contained'];
+const sizeOptions: ButtonProps['size'][] = ['md'];
+
+const meta: Meta<typeof ButtonComponent> = {
+  title: 'Components/Button',
+  component: ButtonComponent,
+  args: {
+    children: 'Button',
+    variant: 'text',
+    size: 'md',
+  },
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    variant: {
+      control: 'select',
+      options: variantOptions,
+    },
+    size: {
+      control: 'select',
+      options: sizeOptions,
+    }
+  },
+  render: ({children, ...args}) => <ButtonComponent {...args}>{children}</ButtonComponent>,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ButtonComponent>;
+
+export const Button: Story = {};
+
+export const ContainedButton: Story = {
+  args: {
+    children: "Button",
+    variant: "contained",
+    size: "md"
+  }
+};
+
+export const TextButton: Story = {
+  args: {
+    children: "Button",
+    variant: "text",
+    size: "md"
+  }
+};
--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -2,13 +2,13 @@ import type { FC, ReactNode } from "react";
 
 import styles from './Button.module.css'
 
-type Button = {
+export type ButtonProps = {
   variant?: "text" | "contained";
   size?: "md";
   children: ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: FC<Button> = ({ variant = "text", size = "md", children, ...props }) => {
+export const Button: FC<ButtonProps> = ({ variant = "text", size = "md", children, ...props }) => {
   return (
     <button className={styles.button} data-size={size} data-variant={variant} {...props}>
       {children}
