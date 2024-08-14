6fe3cfbb11bbe1a4abe350d820ba208f5bd78174
Pawel1894 <pawepohl@gmail.com>
2024-08-14 18:40:19 +0200

    client: install react toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import {
 import { router } from "./routes/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient()
 
@@ -12,6 +14,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer />
     </QueryClientProvider>
   )
 }
