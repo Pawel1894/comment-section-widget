dbaca12be487a18a8e0431e08619cf8e42912897
Pawel1894 <pawepohl@gmail.com>
2024-08-15 11:01:09 +0200

    add typing for use create topic request

--- client/src/topic/hooks/use-create-topic.ts
+++ client/src/topic/hooks/use-create-topic.ts
@@ -1,8 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
 import { axiosInstance } from "src/axiosInstance";
+import { Topic, TopicSchema } from "../topic-types";
 
-const createTopic = async (topic: string): Promise<void> => {
-  await axiosInstance.put("/topic", { content: topic });
+const createTopic = async (topic: string): Promise<ReadonlyArray<Topic>> => {
+  const response = await axiosInstance.put("/topic", { content: topic });
+  return TopicSchema.array().parse(response.data);
 };
 
 type UseCreateTopic = {
